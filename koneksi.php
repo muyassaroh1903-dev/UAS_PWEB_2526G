fad110f71f1230c7a85a7348b407f5d47d000aae
muyassaroh1903-dev <muyassaroh1903@gmail.com>
2026-06-28 20:13:07 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_mahasiswa";
+
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+
+if (!$conn) {
+    die("Koneksi database gagal : " . mysqli_connect_error());
+}
+?>
